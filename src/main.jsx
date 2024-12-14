baa88f966f5b36d143e94bf1026af79a4175edda
Sebastian Silva <sebastian@fuentelibre.org>
2024-12-13 19:26:04 -0500

    Simplificar

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,15 @@
-import { render } from 'preact'
-import  * as ex from "excalibur";
+import * as ex from "excalibur";
+import { Player } from "./player";
 import "./index.css";
-import { App } from './app.jsx'
+import { idleMan } from "./resources";
+ex.Flags.useCanvasGraphicsContext();
 
-render(<App />, document.getElementById('app'))
-
-/* 
 const game = new ex.Engine({
-  width: 400,
-  height: 500,
-  backgroundColor: ex.Color.fromHex("#54C0CA"),
   pixelArt: true,
-  pixelRatio: 2,
-  displayMode: ex.DisplayMode.FitScreen,
+  canvasElementId: "game",
+  displayMode: ex.DisplayMode.FitContainerAndFill
 });
-game.start();
+const loader = new ex.Loader([idleMan])
 
-*/
+game.add(new Player())
+game.start(loader)
